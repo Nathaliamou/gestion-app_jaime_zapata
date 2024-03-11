f55aaeab6c51dd84a2c660bc83d8d7c26d1ed16c
jaimezzapata <disenoydesarrollo@academicos.cesde.edu.co>
2024-03-11 10:22:10 -0500

    Se actualiza estado de login

--- src/components/layouts/auth/Login.jsx
+++ src/components/layouts/auth/Login.jsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
 const Login = () => {
-    return(
-        <form action="">
-            <section>
-                <input placeholder="Usuario" type="text" />
-                <input placeholder="Contraseña" type="text" />
-            </section>
-            <button type="button">Iniciar Sesión</button>
-        </form>
-    )   
-}
-export default Login
+  const [getUsuario, setUsuario] = useState("");
+  const [getContrasena, setContrasena] = useState("");
+  function validarInicioSesion() {
+    if (getUsuario == "Jaime" && getContrasena == "1234") {
+      console.log("Inicio de sesión correcta");
+    } else {
+        console.log('Error de credenciales')
+    }
+  }
+  return (
+    <form action="">
+      <section>
+        <input
+          onChange={(e) => setUsuario(e.target.value)}
+          placeholder="Usuario"
+          type="text"
+        />
+        <input
+          onChange={(e) => setContrasena(e.target.value)}
+          placeholder="Contraseña"
+          type="text"
+        />
+      </section>
+      <button onClick={validarInicioSesion} type="button">Iniciar Sesión</button>
+    </form>
+  );
+};
+export default Login;
