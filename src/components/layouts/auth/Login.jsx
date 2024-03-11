256248643a79eab57fec5d29595edb7d9b6dacef
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2024-03-11 09:18:07 -0500

    Se crea proyecto de gestion

---
+++ src/components/layouts/auth/Login.jsx
@@ -0,0 +1,12 @@
+const Login = () => {
+    return(
+        <form action="">
+            <section>
+                <input placeholder="Usuario" type="text" />
+                <input placeholder="Contraseña" type="text" />
+            </section>
+            <button type="button">Iniciar Sesión</button>
+        </form>
+    )   
+}
+export default Login
