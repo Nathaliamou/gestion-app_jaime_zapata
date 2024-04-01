127e25387f1769a5da98122f84c787d090f0a079
Bernardo A Mira García <bamira1@misena.edu.co>
2024-04-01 08:58:13 -0500

    Se configura react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import Login from "./components/layouts/auth/Login"
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Login from "./components/layouts/auth/Login";
+import Home from "./components/layouts/pages/Home";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: '/home',
+    element: <Home />
+  }
+]);
 
 function App() {
   return (
     <>
-      <Login />
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
